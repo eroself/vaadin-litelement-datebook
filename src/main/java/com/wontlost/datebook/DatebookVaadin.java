18100b207dc67ec7cfad8573d2bc62bb3ffa138e
Ryan Pang <ryan.pang@live.com>
2021-02-20 19:22:57 +1300

    Update

--- src/main/java/com/wontlost/datebook/DatebookVaadin.java
+++ src/main/java/com/wontlost/datebook/DatebookVaadin.java
@@ -4,53 +4,33 @@ import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
 
-import java.util.Optional;
-import java.util.Random;
-
 import javax.validation.constraints.NotNull;
+import java.util.Optional;
 
 @Tag("vaadin-datebook")
 @JsModule("./vaadin-datebook.js")
 public class DatebookVaadin extends Button {
 
-
-    private String id;
-
     /**
      * id will be override to 'video' if set From.camera
      */
     public DatebookVaadin() {
-        this("datebook");
+        setId("datebook");
+        setText("Add to calendar");
     }
 
     /**
      * id will be override to 'video' if set From.camera
      */
-    public DatebookVaadin(@NotNull String id) {
-        setId(id);
+    public DatebookVaadin(@NotNull String text) {
+        setId("datebook");
+        setText(text);
     }
 
     @Override
-    public Optional<String> getId() {
-        return Optional.ofNullable(id);
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-        getElement().setProperty("id", id==null || id.length()==0? "datebook_"+Math.abs(new Random().nextInt()): id);
-    }
-
-    @Override
-    public void setWidth(String width) {
-        super.setWidth(width);
-        getElement().setProperty("width", width==null ? "100%" : width);
-    }
-
-    @Override
-    public void setHeight(String height) {
-        super.setHeight(height);
-        getElement().setProperty("height", height==null ? "100%" : height);
+    public void setText(String text) {
+        super.setText(text);
+        getElement().setProperty("buttonText", Optional.of(text).orElse("Add to calendar"));
     }
 
     public void setCalendarOptions(BasicConfig config) {
--- src/main/resources/META-INF/frontend/vaadin-datebook.js
+++ src/main/resources/META-INF/frontend/vaadin-datebook.js
@@ -5,39 +5,14 @@ class VaadinDatebook extends LitElement {
 
     constructor() {
         super();
-        //
-        // this.calendarOptions = {
-        //     title: 'Happy Hour',
-        //     location: 'The Bar, New York, NY',
-        //     description: 'Let\'s blow off some steam with a tall cold one!',
-        //     start: new Date('2021-02-18T23:30:00'),
-        //     end: new Date('2021-02-18T23:50:00'),
-        //     // an event that recurs every two weeks:
-        //     recurrence: {
-        //         frequency: 'WEEKLY',
-        //         interval: 2
-        //     }
-        // };
-        //
-        // this.calendarAlarm = {
-        //     action: 'DISPLAY',
-        //     trigger: new Date('2021-02-18T23:29:00Z'),
-        //     description: 'the event description',
-        //     summary: 'a quick summary',
-        //     duration: {
-        //         after: true,
-        //         minutes: 3
-        //     }
-        // }
+        this.addEventListener('click', this.clickHandler);
     }
 
     static get properties() {
-        return { id: String,
-                 width: String,
-                 height: String,
+        return { buttonText: String,
                  calendarOptions: Object,
                  calendarAlarm: Object
-                };
+        };
     }
 
     createRenderRoot() {
@@ -52,14 +27,8 @@ class VaadinDatebook extends LitElement {
         icalendar.download();
     }
 
-    firstUpdated(changedProperties) {
-        super.firstUpdated(changedProperties);
-        // const icalendar = new Datebook.ICalendar(this.calendarOptions);
-        // icalendar.addAlarm(this.calendarAlarm).render();
-    }
-
     render() {
-        return html`<button id="${this.id}" @click=${this.clickHandler}>Add to Calendar</button>`;
+        return html`${this.buttonText}`;
     }
 
 }
